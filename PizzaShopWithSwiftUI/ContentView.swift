b602383761f3d6c957f723a2810d2c7e3ddb9ad3
Nikolay Vinogradov <nikolya0209@mail.ru>
2022-03-02 14:01:57 +0300

    Initial Commit

---
+++ PizzaShopWithSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PizzaShopWithSwiftUI
+//
+//  Created by MacBookPro on 02.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PizzaShopWithSwiftUI/PizzaShopWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaShopWithSwiftUIApp.swift
+//  PizzaShopWithSwiftUI
+//
+//  Created by MacBookPro on 02.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct PizzaShopWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

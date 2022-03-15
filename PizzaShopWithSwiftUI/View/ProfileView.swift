9d1cf21dee690bd8955ac7d96113e7de94911d2f
Nikolay Vinogradov <nikolya0209@mail.ru>
2022-03-15 19:32:04 +0300

    UI ProfileView

--- PizzaShopWithSwiftUI/View/ProfileView.swift
+++ PizzaShopWithSwiftUI/View/ProfileView.swift
@@ -9,7 +9,33 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text("Профиль")
+        
+        VStack(alignment: .leading, spacing: 20) {
+        
+        HStack(spacing: 16) {
+            Image("user")
+                .resizable()
+                .frame(width: 80, height: 80)
+                .padding(8)
+                .background(Color("lightGray"))
+                .clipShape(Circle())
+            
+            VStack(alignment: .leading, spacing: 12) {
+                Text("Владислав Иванович")
+                    .bold()
+                Text("+7 329 23 322 23")
+            }
+        }
+            
+            VStack(alignment: .leading, spacing: 2) {
+            Text("Адрес доставки: ")
+                .bold()
+            Text("Беларусь, г. Гродно, ул. Василька, дом 9")
+            }
+            List{
+                
+            }.listStyle(.plain)
+        }
     }
 }
 

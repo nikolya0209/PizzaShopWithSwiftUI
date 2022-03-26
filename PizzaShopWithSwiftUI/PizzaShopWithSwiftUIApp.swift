0e7bc46b9a69fbf483db7d0ddda06136cd6b5a72
Nikolay Vinogradov <nikolya0209@mail.ru>
2022-03-26 22:38:30 +0300

    AuthService

--- PizzaShopWithSwiftUI/PizzaShopWithSwiftUIApp.swift
+++ PizzaShopWithSwiftUI/PizzaShopWithSwiftUIApp.swift
@@ -6,14 +6,23 @@
 //
 
 import SwiftUI
+import Firebase
 
 let screen = UIScreen.main.bounds
 
 @main
 struct PizzaShopWithSwiftUIApp: App {
+    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
     var body: some Scene {
         WindowGroup {
             AuthView()
+        }
+    }
+    class AppDelegate: NSObject, UIApplicationDelegate {
+        func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+            
+            FirebaseApp.configure()
+            return true
         }
     }
 }
---
+++ PizzaShopWithSwiftUI/Services/AuthService.swift
@@ -0,0 +1,32 @@
+//
+//  AuthService.swift
+//  PizzaShopWithSwiftUI
+//
+//  Created by MacBookPro on 26.03.2022.
+//
+
+import Foundation
+import FirebaseAuth
+
+class AuthService {
+    static let shared = AuthService()
+    
+    private init() { }
+    
+    private let auth = Auth.auth()
+    
+    private var currentUser: User? {
+        return auth.currentUser
+    }
+    
+    func signUP(email: String, password: String, completion: @escaping (Result<User, Error>) -> ()) {
+        auth.createUser(withEmail: email, password: password) { result, error in
+            if let result = result {
+                completion(.success(result.user))
+            } else if let error = error {
+                completion(.failure(error))
+            }
+        }
+    }
+    
+}

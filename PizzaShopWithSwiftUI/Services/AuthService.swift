3cafcad60549263f84cf25737331c7ae248b4d7b
Nikolay Vinogradov <nikolya0209@mail.ru>
2022-03-28 20:36:16 +0300

    func authorization

--- PizzaShopWithSwiftUI/Services/AuthService.swift
+++ PizzaShopWithSwiftUI/Services/AuthService.swift
@@ -29,4 +29,14 @@ class AuthService {
         }
     }
     
+    func signIn(email: String, password: String, completion: @escaping (Result<User, Error>) -> ()) {
+        auth.signIn(withEmail: email, password: password) { result, error in
+            if let result = result {
+                completion(.success(result.user))
+            } else if let error = error {
+                completion(.failure(error))
+            }
+        }
+    }
+    
 }

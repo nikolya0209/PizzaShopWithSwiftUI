2b1a6df8988e6660981bd7ee5e7f51edfd7d9260
Nikolay Vinogradov <nikolya0209@mail.ru>
2022-03-30 17:04:37 +0300

    user model

---
+++ PizzaShopWithSwiftUI/Model/MvUser.swift
@@ -0,0 +1,15 @@
+//
+//  MvUser.swift
+//  PizzaShopWithSwiftUI
+//
+//  Created by MacBookPro on 30.03.2022.
+//
+
+import Foundation
+
+struct MvUser: Identifiable {
+    var id: String
+    var name: String
+    var phone: Int
+    var adress: String
+}

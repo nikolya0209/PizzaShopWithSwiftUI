5a73ad7ddac34f4938fae0625347351c4d2cd7ab
Nikolay Vinogradov <nikolya0209@mail.ru>
2022-05-09 23:06:06 +0300

    get order in profileViewModel

--- PizzaShopWithSwiftUI/ViewModel/ProfileViewModel.swift
+++ PizzaShopWithSwiftUI/ViewModel/ProfileViewModel.swift
@@ -10,9 +10,23 @@ import SwiftUI
 
 class ProfileViewModel: ObservableObject {
     @Published var profile: MvUser
+    @Published var orders: [Order]
     
     init(profile: MvUser) {
         self.profile = profile
+    }
+    
+    func getOrders() {
+        DatabaseService.shared.getOrders(by: AuthService.shared.currentUser!.accessibilityHint) { result in
+            switch result {
+                
+            case .success(let orders):
+                self.orders = orders
+                print("Всего заказов: \(orders.count)")
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
     
     func setProfile() {

72d06a6fbed14b971cef4f7d49331a280f8a0a66
Nikolay Vinogradov <nikolya0209@mail.ru>
2022-05-14 20:32:03 +0300

    Admin Interface

---
+++ PizzaShopWithSwiftUI/View/AdminInterface/AdminOrdersView.swift
@@ -0,0 +1,20 @@
+//
+//  AdminOrdersView.swift
+//  PizzaShopWithSwiftUI
+//
+//  Created by MacBookPro on 14.05.2022.
+//
+
+import SwiftUI
+
+struct AdminOrdersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AdminOrdersView_Previews: PreviewProvider {
+    static var previews: some View {
+        AdminOrdersView()
+    }
+}

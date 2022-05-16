071bac43046df2d2488aae54d3605416ebdc36dd
Nikolay Vinogradov <nikolya0209@mail.ru>
2022-05-16 21:56:51 +0300

    get orders in AdminOrdersViewModel

--- PizzaShopWithSwiftUI/ViewModel/AdminOrdersViewModel.swift
+++ PizzaShopWithSwiftUI/ViewModel/AdminOrdersViewModel.swift
@@ -8,4 +8,15 @@
 import Foundation
 class AdminOrdersViewModel: ObservableObject {
     @Published var orders = [Order]()
+    
+    func getOrders() {
+        DatabaseService.shared.getOrders(by: nil) { result in
+            switch result {
+            case .success(let orders):
+                self.orders = orders
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
 }

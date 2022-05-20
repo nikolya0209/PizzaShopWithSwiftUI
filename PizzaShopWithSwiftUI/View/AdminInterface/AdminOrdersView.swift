77a2e8c4a8199a44ba1f2dfe31184149d0997da4
Nikolay Vinogradov <nikolya0209@mail.ru>
2022-05-20 21:10:41 +0300

    refactoring AdminOrdersView

--- PizzaShopWithSwiftUI/View/AdminInterface/AdminOrdersView.swift
+++ PizzaShopWithSwiftUI/View/AdminInterface/AdminOrdersView.swift
@@ -9,17 +9,19 @@ import SwiftUI
 
 struct AdminOrdersView: View {
     @StateObject var viewModel = AdminOrdersViewModel()
+    
     var body: some View {
         List {
             ForEach(viewModel.orders, id: \.id) { order in
                 OrderCell(order: order)
-            }.listStyle(.plain)
-                .onAppear {
-                    viewModel.getOrders()
-                }
-        }
+            }
+        }.listStyle(.plain)
+            .onAppear {
+                viewModel.getOrders()
+            }
     }
 }
+
 
 struct AdminOrdersView_Previews: PreviewProvider {
     static var previews: some View {

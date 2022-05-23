e3e2bc9c513098f2144390e94bccf0e72a6d6c1e
Nikolay Vinogradov <nikolya0209@mail.ru>
2022-05-23 20:51:45 +0300

    list in Orderview

--- PizzaShopWithSwiftUI/View/AdminInterface/OrderView.swift
+++ PizzaShopWithSwiftUI/View/AdminInterface/OrderView.swift
@@ -8,13 +8,19 @@
 import SwiftUI
 
 struct OrderView: View {
+    @StateObject var viewModel: OrderViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(viewModel.order.positions, id: \.id) {position in
+                PositionCell(position: position)
+            }
+        }
     }
 }
 
 struct OrderView_Previews: PreviewProvider {
     static var previews: some View {
-        OrderView()
+        OrderView(viewModel: OrderViewModel(order: Order(userID: "", date: Date(), status: "Новый")))
     }
 }
